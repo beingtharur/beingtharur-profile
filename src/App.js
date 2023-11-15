96a7ad35cdfba56a3307e0279916b6e974550783
beingtharur <97608321+beingtharur@users.noreply.github.com>
2023-11-15 16:45:35 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Profile = () => {
   return (
     <div>
-      <h1>beingtharur's Profile</h1>
+      <h1>beingtharur's Profile Update</h1>
       <p>Name: beingtharur</p>
       <p>Skills: JavaScript, React, HTML, CSS</p>
       <p>Bio: I am a passionate coder with expertise in JavaScript and React. I love building web applications and exploring new technologies.</p>
